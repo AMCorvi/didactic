9fd908d7855a25e1eafa60372c21618a22b37d63
A M Corvi <amcorvi@icloud.com>
2017-07-25 01:06:53 -0400

    - Changed: index.pug to utilize layout.pug partial - Created: layout.pug partial - Added: Links to "/login" and "/sign-up" pages - Added: routes for above mentioned links - Added: body-parser and mongoose modules to begin login page process

--- nodeJS/express/basic/lib/app.js
+++ nodeJS/express/basic/lib/app.js
@@ -4,6 +4,10 @@ var _express = require("express");
 
 var _express2 = _interopRequireDefault(_express);
 
+var _bodyParser = require("body-parser");
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 var _path = require("path");
 
 var _path2 = _interopRequireDefault(_path);
@@ -13,20 +17,39 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 //Initialze Express
 var app = (0, _express2.default)();
 
-/**
+/*
  * Plugin View Engine
- * @returns {HTML Template} Using Pug(Jade)
+ * @returns {HTML} Using Pug(Jade)
  */
 app.set("views", _path2.default.join(__dirname, "..", "views"));
-
 app.set("view engine", "pug");
+
+/* Parse request body into json */
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use(_bodyParser2.default.json());
+
+// Link views to static assests
+app.use(_express2.default.static(_path2.default.join(__dirname, "..", "client")));
 
 //Base Route
 app.get("/", function (req, res) {
-  res.render("index");
+    res.render("index", {
+        title: "Everything is Awesome",
+        headingMainText: "Oochie Wally wally Oochie Bang Bang"
+    });
+});
+
+//Login Page Route
+app.get("/login", function (req, res) {
+    res.send("This is the login page");
+});
+
+//Sign-up Page Route
+app.get("/signup", function (req, res) {
+    res.send("This is the sign-up page");
 });
 
 //Spin-up Server
-app.listen(8000, function () {
-  return console.log("Server started on Port :8000");
+app.listen(80, function () {
+    return console.log(" Server is running @ http://localhost");
 });
--- nodeJS/express/basic/src/app.js
+++ nodeJS/express/basic/src/app.js
@@ -1,28 +1,43 @@
 import express from "express";
+import bodyParser from "body-parser";
 import path from "path";
-
 
 //Initialze Express
 const app = express();
 
-
-/**
+/*
  * Plugin View Engine
- * @returns {HTML Template} Using Pug(Jade)
+ * @returns {HTML} Using Pug(Jade)
  */
 app.set("views", path.join(__dirname, "..", "views"));
-
 app.set("view engine", "pug");
 
+/* Parse request body into json */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Link views to static assests
+app.use(express.static(path.join(__dirname, "..", "client")));
 
 //Base Route
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        title: "Everything is Awesome"
+      , headingMainText: "Oochie Wally wally Oochie Bang Bang"
+    });
+});
+
+//Login Page Route
+app.get("/login", (req, res) => {
+    res.send("This is the login page");
+});
+
+//Sign-up Page Route
+app.get("/signup", (req, res) => {
+    res.send("This is the sign-up page");
 });
 
 
 //Spin-up Server
-app.listen(8000, () => console.log("Server started on Port :8000"));
-
+app.listen(80, () => console.log(" Server is running @ http://localhost"));
 
